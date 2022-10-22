0ddafa45bbe655ed1966e72f3637c10ccfe84ace
dongwei <598644648@qq.com>
2022-10-22 15:59:29 +0800

    集成日志和nacos 访问nacos配置文件

--- ashura-web/src/main/java/com/example/ashura/web/AshuraWebApplication.java
+++ ashura-web/src/main/java/com/example/ashura/web/AshuraWebApplication.java
@@ -1,6 +1,8 @@
 package com.example.ashura.web;
 
 import org.apache.catalina.Group;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.alibaba.nacos.api.NacosFactory;
@@ -10,22 +12,24 @@ import java.util.Properties;
 
 @SpringBootApplication
 public class AshuraWebApplication {
+    private static final Logger logger = LoggerFactory.getLogger(AshuraWebApplication.class);
 
-    public static void main(String[] args) {
-        //nacos 地址
-        String serverAddr = "http://124.223.2.193:8848/";
+    public static void main(String[] args) throws NacosException {
+         //nacos 地址
+        String serverAddr = "http://124.223.2.193:8848";
         //Data Id
          String dataId = "ashura-web.ymal";
        // Group
         String group = "DEFAULT_GROUP";
         Properties properties = new Properties();
         properties.put("serverAddr",serverAddr);
+      //  properties.put("namespace","public");
         ConfigService configService = NacosFactory.createConfigService(properties);
         //获取配置,String dataId, String group, long timeoutMs
          String content = configService.getConfig(dataId, group, 5000);
-         System.out.println(content);
+        logger.info(content);
         SpringApplication.run(AshuraWebApplication.class, args);
-        System.out.println("启动成功");
+        logger.info("启动成功");
     }
 
 }
--- ashura-web/src/test/java/com/example/ashura/web/AshuraWebApplicationTests.java
+++ ashura-web/src/test/java/com/example/ashura/web/AshuraWebApplicationTests.java
@@ -3,11 +3,68 @@ package com.example.ashura.web;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @SpringBootTest
 class AshuraWebApplicationTests {
 
     @Test
     void contextLoads() {
     }
+
+//    /**
+//     * jdk11 新特性示例
+//     */
+//    @Test
+//    public void jdk11Test(){
+//        // 1.用来判断字符串是不是空字符"“或者trim()之后(” ")为空字符:
+//        String blankStr = "    ";
+//        // true
+//        boolean trueVal = blankStr.isBlank();
+//
+//        //2 . 将一个字符串按照行终止符（换行符\n或者回车符\r）进行分割，并将分割为Stream流:
+//        String newStr = "Hello Java 11 \n felord.cn \r 2021-09-28";
+//
+//        Stream<String> lines = newStr.lines();
+//        lines.forEach(System.out::println);
+//
+//
+//        // 3.按照给定的次数重复串联字符串的内容：
+//        String str = "HELLO";
+//        // 空字符
+//        String empty = str.repeat(0);
+//        // HELLO
+//        String repeatOne = str.repeat(1);
+//        // HELLOHELLO
+//        String repeatTwo = str.repeat(2);
+//
+//       //4. 集合转对应类型的数组
+//        List<String> sampleList = Arrays.asList("张三", "java 11");
+//        // array = {"张三", "java 11"};
+//        String[] array = sampleList.toArray(String[]::new);
+//
+//
+//        List<String> sList = Arrays.asList("张三", "java 11","jack");
+//        // 断言取反
+//        List<String> result = sList.stream()
+//                // 过滤以j开头的字符串
+//                .filter(s -> s.startsWith("j"))
+//                // 同时不包含11的字符串
+//                .filter(Predicate.not(s -> s.contains("11")))
+//                .collect(Collectors.toList());
+//        // var可以用于修饰Lambda局部变量
+//        List<String> abList = Arrays.asList("张三", "java 11","jack");
+//        List<String> result2 = abList.stream()
+//                // 过滤以j开头的字符串
+//                .filter((@NotNull var s) -> s.startsWith("j"))
+//                // 同时不包含11的字符串
+//                .filter(Predicate.not((@NotNull var s) -> s.contains("11")))
+//                .collect(Collectors.toList());
+//
+//    }
 
 }
